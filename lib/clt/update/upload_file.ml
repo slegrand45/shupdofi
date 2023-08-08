16ee3030a8409c9ab708b1f455264e918d44f580
Stephane Legrand <slegrand45@gmail.com>
2023-08-08 22:29:18 +0200

    json -> txt

--- lib/clt/update/upload_file.ml
+++ lib/clt/update/upload_file.ml
@@ -33,21 +33,21 @@ let update m a =
       let () = Js_toast.show ~document ~toast_id in
       let filename = Js_browser.File.name file in
       let url = Routing.Api.(to_url ~encode:(fun e -> Js_of_ocaml.Js.(to_string (encodeURIComponent (string e)))) (Upload { area_id; area_subdirs; filename })) in
-      let c = [Api.http_post_file ~url ~file (fun status json -> Action.Upload_file (Action_other.Upload_file.Done { toast_id; status; json; filename }))] in
+      let c = [Api.http_post_file ~url ~file (fun status txt -> Action.Upload_file (Action_other.Upload_file.Done { toast_id; status; txt; filename }))] in
       return m ~c
     )
-  | Action_other.Upload_file.Done { toast_id; status; json; filename } -> (
+  | Action_other.Upload_file.Done { toast_id; status; txt; filename } -> (
       let m =
         match status with
         | 201 ->
-          let uploaded = Yojson.Safe.from_string json |> Msg_from_srv.Uploaded.t_of_yojson in
+          let uploaded = Yojson.Safe.from_string txt |> Msg_from_srv.Uploaded.t_of_yojson in
           Js_toast.set_status_ok ~doc:Dom_html.document ~id:toast_id ~delay:5.0;
           let area_id = Msg_from_srv.Uploaded.get_area_id uploaded in
           let subdirs = Msg_from_srv.Uploaded.get_subdirs uploaded in
           let file = Msg_from_srv.Uploaded.get_file uploaded in
           { m with area_content = Com.Area_content.(add_uploaded ~id:area_id ~subdirs ~file m.area_content |> sort) }
         | _ ->
-          Js_toast.set_status_ko ~doc:Dom_html.document ~id:toast_id ~msg:("Unable to upload file " ^ filename);
+          Js_toast.set_status_ko ~doc:Dom_html.document ~id:toast_id ~msg:("Unable to upload file " ^ filename ^ ": " ^ txt);
           m
       in
       let () = Js_toast.clean_hiddens ~document in
