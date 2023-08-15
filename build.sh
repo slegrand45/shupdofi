260119ecd166993258b76ae7ea1481492f8dc233
Stephane Legrand <slegrand45@gmail.com>
2023-08-15 16:19:18 +0200

    Use shupdofi name instead of main name

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 find ./bin/ -name '*.ml*' | xargs ocp-indent -i && find ./lib/ -name '*.ml*' | xargs ocp-indent -i \
 && dune build --profile release \
-&& cp -v -f _build/default/bin/clt/main.bc.js www/js/ \
-&& gzip -vkf9 www/js/main.bc.js
+&& cp -v -f _build/default/bin/clt/shupdofi.bc.js www/js/ \
+&& gzip -vkf9 www/js/shupdofi.bc.js
 
 # && dune build --profile release \
