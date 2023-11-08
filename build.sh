9dd435c2729428da3b2aca1301acc020fe682adf
Stephane Legrand <slegrand45@gmail.com>
2023-11-08 22:54:15 +0100

    Rework background color and video

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 find ./bin/ -name '*.ml*' | xargs ocp-indent -i && find ./lib/ -name '*.ml*' | xargs ocp-indent -i \
-&& dune build \
+&& dune build --profile release \
 && cp -v -f _build/default/bin/clt/shupdofi.bc.js www/js/ \
 && gzip -vkf9 www/js/shupdofi.bc.js
 
